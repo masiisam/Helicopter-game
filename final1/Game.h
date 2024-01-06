f715441528ca8add50b15b8674837bbaf049c5e0
masiisam <155811690+masiisam@users.noreply.github.com>
2024-01-06 18:26:38 +0330

    Update Game.h

--- final1/Game.h
+++ final1/Game.h
@@ -1,29 +1,3 @@
-// #ifndef GAME_H
-// #define GAME_H
-
-// #include <QGraphicsView>
-// #include <QGraphicsItem>
-// #include <QGraphicsScene>
-// #include <QMenu>
-// #include <QMenuBar>
-// #include "Player.h"
-// #include "Score.h"
-// #include "Health.h"
-// class Game: public QGraphicsView{
-// public:
-//     Game(QGraphicsItem * parent=0);
-
-//     QGraphicsScene * scene;
-//     Player * player;
-//     Score * score;
-//     Health * health;
-// public slots:
-//     void newGame();
-//     void saveGame();
-//     void loadGame();
-// };
-
-// #endif // GAME_H
 #ifndef GAME_H
 #define GAME_H
 
